c4d55d31cfd6263b15a0c34a4d6434ccb80c500a
unknown <ew913@eews304-026.ic.ac.uk>
2016-12-14 17:19:06 +0000

    Q1_c

--- coursework2.m
+++ coursework2.m
@@ -1,4 +1,7 @@
-%%Q1)A) la lalalala lalala
+clear all;
+close all;
+clc;
+%% Q1)A) la lalalala lalala
 load wine.data.csv;
 classIdentifier = wine_data(:,1);
 wineData = wine_data(:,2:14);
@@ -11,7 +14,7 @@ normWineDataC1 = normr(wineDataC1);
 normWineDataC2 = normr(wineDataC2);
 normWineDataC3 = normr(wineDataC3);
 
-%%Q1)B) wakakakakakaka 
+%% Q1)B) wakakakakakaka 
 
 covMatrixAll = cov(wineData);
 covMatrixAllNorm = cov(normWineData);
@@ -23,4 +26,72 @@ covMatrixC1Norm = cov(normWineDataC1);
 covMatrixC2Norm = cov(normWineDataC2);
 covMatrixC3Norm = cov(normWineDataC3);
 
-%%Q1)C) 
+%% Q1)C)
+
+figure;
+scatter (normWineDataC1(:, 5), normWineDataC1(:, 13),30, 'b', 'x');
+hold on;
+scatter (normWineDataC2(:, 5), normWineDataC2(:, 13),30, 'r');
+hold on;
+scatter (normWineDataC3(:, 5), normWineDataC3(:, 13),30, 'k', '^');
+hold on;
+xlabel('Dimension 5');
+ylabel('Dimension 13');
+title('Dimensions with largest covariance');
+
+[V1, D1] = eig(covMatrixC1Norm);
+V1_selected = [V1(5, 13), V1(13, 13)];
+
+p1 = [0.1, 0.99];
+p2 = p1 + 0.01*V1_selected;
+dp = 0.03*V1_selected;
+%quiver(V1(5, 13), V1(13, 13), 0.1);
+quiver(p1(1),p1(2),dp(1),dp(2),0, 'b');
+
+[V2, D2] = eig(covMatrixC2Norm);
+V2_selected = [V2(5, 13), V2(13, 13)];
+
+p1 = [0.2, 0.97];
+p2 = p1 + 0.01*V2_selected;
+dp = 0.03*V2_selected;
+%quiver(V1(5, 13), V1(13, 13), 0.1);
+quiver(p1(1),p1(2),dp(1),dp(2),0, 'r');
+
+[V3, D3] = eig(covMatrixC3Norm);
+V3_selected = [V3(5, 13), V3(13, 13)];
+
+p1 = [0.17, 0.98];
+p2 = p1 + 0.01*V3_selected;
+dp = 0.03*V3_selected;
+%quiver(V1(5, 13), V1(13, 13), 0.1);
+quiver(p1(1),p1(2),dp(1),dp(2),0, 'k');
+
+
+legend('Class 1','Class 2','Class 3', 'Class 1 Eigenvector', 'Class 2 Eigenvector', 'Class 3 Eigenvector') ;
+hold off;
+
+min = 9999999;
+for i = 1:13
+    for j=1:13
+        if min > abs(covMatrixC3(i, j))
+            min = abs(covMatrixC3(i, j));
+            x_min = i; y_min = j;
+        end
+    end
+end
+
+
+figure;
+scatter (normWineDataC1(:, 7), normWineDataC1(:, 11));
+hold on;
+scatter (normWineDataC2(:, 7), normWineDataC2(:, 11));
+hold on;
+scatter (normWineDataC3(:, 7), normWineDataC3(:, 11));
+hold off;
+legend('Class 1','Class 2','Class 3') ;
+xlabel('Dimension 7');
+ylabel('Dimension 11');
+title('Dimensions with lowest covariance');
+
+%end
+%scatter (covMatrixC1(1, :), covMatrixC1(2, :));
